5196a1c3bb1dfe6e852d8a4e1326f2f7db01c6a6
AyoubGharbi27 <ayoubgharbi932@gmail.com>
2024-04-20 01:07:20 +0100

    modified the footer and home page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,6 @@
 import { Routes,Route } from "react-router-dom";
 import 'mdb-react-ui-kit/dist/css/mdb.min.css';
 import "@fortawesome/fontawesome-free/css/all.min.css";
-import 'bootstrap/dist/css/bootstrap.min.css';
 import Home from "./pages/Home";
 import About from "./pages/About";
 import Clubs from "./pages/Clubs";
@@ -9,11 +8,16 @@ import WorkShop from "./pages/WorkShops";
 import Account from "./pages/Account";
 import AppNavbar from "./Components/Navbar/Navbar";
 import Footer from "./Components/Footer/Footer";
+import HVid from "./Components/homepagecomponents/homeVid";
+import GridLay from "./Components/homepagecomponents/gridLay";
+
 function App() {
 
   return (
     <>
       <AppNavbar />
+      <HVid />
+      <GridLay />
       < Routes>
         <Route path="/"  element={<Home />} />
         <Route path="/about"  element={<About />} />
